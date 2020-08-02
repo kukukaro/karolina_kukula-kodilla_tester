4d0243e8a36dce6b0b1b26d5b8df52d1a7c5650f
Karolina Kukula <kukukaro@gmail.com>
2020-08-02 20:19:27 +0200

    Przygotwanie klas startowych

---
+++ kodilla-intro/src/main/java/HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("Karolina");
+        System.out.println(28);
+        System.out.println('K');
+    }
+}
