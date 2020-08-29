7430ff48fefadb89c67b1febaa7e781726e27985
Karolina Kukula <kukukaro@gmail.com>
2020-08-29 13:51:06 +0200

    Instrukcje warunkowe

--- kodilla-intro/src/main/java/FirstClass.java
+++ kodilla-intro/src/main/java/FirstClass.java
@@ -2,6 +2,14 @@ public class FirstClass {
     public static void main(String[] args) {
         String hello = "Hello from FirstClass";
         System.out.println(hello);
-    }
 
+
+        if (1 > 2) {
+            System.out.println("Niesamowite! Jeden jest większa niż dwa!!!");
+        } else {
+            System.out.println("Wszystko po staremu! Jeden jest mniejsze niż dwa.");
+        }
+
+
+    }
 }
---
+++ kodilla-intro/src/main/java/LeapYear.java
@@ -0,0 +1,22 @@
+public class LeapYear {
+    public static void main(String[] args) {
+        int year = 350;
+        System.out.println(isLeap(year));
+        System.out.println(isLeap2(year));
+    }
+
+    public static boolean isLeap(int year){
+        if(year%4 != 0){
+            return false;
+        } else if(year%100 != 0){
+            return true;
+        } else if(year%400 == 0){
+            return true;
+        }
+        return false;
+    }
+
+    public static boolean isLeap2(int year){
+        return ((year%4 == 0 && year%100 != 0) || (year%400 == 0));
+    }
+}
