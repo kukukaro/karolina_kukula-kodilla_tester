c9db4b1e1d4d5450807b3a2f0f170f5d187266e6
Karolina Kukula <kukukaro@gmail.com>
2020-09-17 21:10:44 +0200

    Modul 3 - 3.5 Testy jednostkowe

---
+++ kodilla-basic-tests/src/main/java/com/kodilla/basic_assertion/Calculator.java
@@ -0,0 +1,19 @@
+package com.kodilla.basic_assertion;
+
+public class Calculator {
+    public Calculator() {
+    }
+
+    public int sum(int a, int b){
+        return (a + b);
+    }
+
+    public int subtract(int a, int b){
+        return (a - b);
+    }
+
+    public int square(int a){
+        return (a * a);
+    }
+
+}
