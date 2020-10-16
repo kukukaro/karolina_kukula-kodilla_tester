87369d1151a9b7fdb29c4385bcf69519f9948c7a
Karolina Kukula <kukukaro@gmail.com>
2020-10-16 20:31:58 +0200

    Modul 7 - 7.2 - CsvFileSource - GamblingMachineTest -> 4 tests failed

--- kodilla-advanced-tests/src/test/java/com/kodilla/parametrized_tests/homework/GamblingMachineTest.java
+++ kodilla-advanced-tests/src/test/java/com/kodilla/parametrized_tests/homework/GamblingMachineTest.java
@@ -1,24 +1,75 @@
 package com.kodilla.parametrized_tests.homework;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvFileSource;
-import org.junit.jupiter.params.provider.CsvSource;
 
+import java.util.Collections;
 import java.util.HashSet;
 import java.util.Set;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 class GamblingMachineTest {
 
     GamblingMachine gamblingMachine = new GamblingMachine();
 
     @ParameterizedTest
-    @CsvFileSource(resources = "/gamblingGameExamples.csv", delimiter = ':')
-    public void shouldRetunMatchedResultForCorrectValues(String testNumbers) throws InvalidNumbersException {
+    @CsvFileSource(resources = "/gamblingMachineExamplesCorrect.csv", delimiter = ':')
+    public void shouldReturnMatchedResultForCorrectValues(String testNumbers) throws InvalidNumbersException {
         Set<Integer> numbers = convertToSet(testNumbers);
 
         int checked = gamblingMachine.howManyWins(numbers);
+    }
+
+    @ParameterizedTest
+    @CsvFileSource(resources = "/gamblingMachineTooShortExample.csv", delimiter = ':')
+    public void shouldReturnExceptionForTooShortSet(String testNumbers) {
+        Assertions.assertThrows(InvalidNumbersException.class, () -> {
+            Set<Integer> numbers = convertToSet(testNumbers);
+
+            int checked = gamblingMachine.howManyWins(numbers);
+        });
+
+    }
+
+    @ParameterizedTest
+    @CsvFileSource(resources = "/gamblingMachineTooLongExample.csv", delimiter = ':')
+    public void shouldReturnExceptionForTooLongSet(String testNumbers) {
+        Assertions.assertThrows(InvalidNumbersException.class, () -> {
+            Set<Integer> numbers = convertToSet(testNumbers);
+
+            int checked = gamblingMachine.howManyWins(numbers);
+        });
+    }
+
+    @ParameterizedTest
+    @CsvFileSource(resources = "/gamblingMachineZeroAndNegativeValues.csv", delimiter = ':')
+    public void shouldReturnExceptionForZeroAndNegativeValue(String testNumbers) {
+        Assertions.assertThrows(InvalidNumbersException.class, () -> {
+            Set<Integer> numbers = convertToSet(testNumbers);
+
+            int checked = gamblingMachine.howManyWins(numbers);
+        });
+    }
+
+    @ParameterizedTest
+    @CsvFileSource(resources = "/gamblingMachineTooHighValues.csv", delimiter = ':')
+    public void shouldReturnExceptionForTooHighValues(String testNumbers) {
+        Assertions.assertThrows(InvalidNumbersException.class, () -> {
+            Set<Integer> numbers = convertToSet(testNumbers);
+
+            int checked = gamblingMachine.howManyWins(numbers);
+        });
+    }
+
+    @Test
+    public void shouldReturnExceptionForEmptyValues() {
+        Assertions.assertThrows(InvalidNumbersException.class, () -> gamblingMachine.howManyWins(Collections.emptySet()));
+    }
+
+    @Test
+    public void shouldReturnExceptionForNullValues() {
+        Assertions.assertThrows(InvalidNumbersException.class, () -> gamblingMachine.howManyWins(null));
     }
 
     private Set<Integer> convertToSet(String testNumbers) {
