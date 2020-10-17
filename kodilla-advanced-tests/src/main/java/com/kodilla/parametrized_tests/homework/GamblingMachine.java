731ea35e35ed0919eaafcf551b2e50a9597ca450
Karolina Kukula <kukukaro@gmail.com>
2020-10-17 08:57:36 +0200

    Modul 7 - 7.2 - CsvFileSource - GamblingMachineTest -> All tests passed

--- kodilla-advanced-tests/src/main/java/com/kodilla/parametrized_tests/homework/GamblingMachine.java
+++ kodilla-advanced-tests/src/main/java/com/kodilla/parametrized_tests/homework/GamblingMachine.java
@@ -6,7 +6,7 @@ import java.util.Set;
 
 public class GamblingMachine {
 
-        public int howManyWins(Set<Integer> userNumbers) throws InvalidNumbersException {
+    public int howManyWins(Set<Integer> userNumbers) throws InvalidNumbersException {
         validateNumbers(userNumbers);
         Set<Integer> computerNumbers = generateComputerNumbers();
         int count = 0;
@@ -18,22 +18,23 @@ public class GamblingMachine {
         return count;
     }
 
-    private void validateNumbers(Set<Integer> numbers) throws InvalidNumbersException {
-        if (numbers.size() != 6) {
-            throw new InvalidNumbersException();
-        }
-
-        if (numbers.stream().anyMatch(number -> number < 1)) {
-            throw new InvalidNumbersException();
-        }
-    }
-
     private Set<Integer> generateComputerNumbers() {
         Set<Integer> numbers = new HashSet<>();
         Random generator = new Random();
-        while(numbers.size() < 6) {
+        while (numbers.size() < 6) {
             numbers.add(generator.nextInt(49) + 1);
         }
         return numbers;
+    }
+
+    private void validateNumbers(Set<Integer> numbers) throws InvalidNumbersException {
+
+        if (numbers == null || numbers.size() != 6) {
+            throw new InvalidNumbersException();
+        }
+
+        if (numbers.stream().anyMatch(number -> (number < 1 || number > 50))) {
+            throw new InvalidNumbersException();
+        }
     }
 }
