e85370ec4809ac2ab986fd86d2702a2e8f68f976
Karolina Kukula <kukukaro@gmail.com>
2020-10-23 12:17:24 +0200

    Modul 7 - 7.6 - Mockito - WeatherAlertApp - test

--- kodilla-advanced-tests/src/main/java/com/kodilla/mockito/homework/WeatherAlertApp.java
+++ kodilla-advanced-tests/src/main/java/com/kodilla/mockito/homework/WeatherAlertApp.java
@@ -16,26 +16,30 @@ public class WeatherAlertApp {
 
 
     public void sendNotificationForAllUsers(Notification notification) {
-        Set<User> notDuplicatedUsers = database.values().stream()
+        database.values().stream()
                 .flatMap(Set::stream)
-                .collect(Collectors.toSet());
-        notDuplicatedUsers.forEach(u -> u.receiveNotification(notification));
-    }
-
-    public void sendNotificationForOneLocationUsers(Location location, Notification notification) {
-        database.get(location)
+                .collect(Collectors.toSet())
                 .forEach(u -> u.receiveNotification(notification));
     }
 
-    public void removeSubscriberFromAllLocations(User user) {
-        for (Set<User> users : database.values()) {
-            users.remove(user);
+    public void sendNotificationForOneLocationUsers(Location location, Notification notification) {
+        if (database.containsKey(location)) {
+            database.get(location)
+                    .forEach(u -> u.receiveNotification(notification));
         }
     }
 
+    public void removeSubscriberFromAllLocations(User user) {
+
+        database.values().stream()
+                .forEach(u -> u.remove(user));
+        }
+
     public void removeSubscriberFromLocation(User user, Location location) {
-        Set<User> users = database.get(location);
-        users.remove(user);
+        if (database.containsKey(location)) {
+            Set<User> users = database.get(location);
+            users.remove(user);
+        }
     }
 
     public void deleteLocation(Location location) {
--- kodilla-advanced-tests/src/test/java/com/kodilla/mockito/homework/WeatherAlertApplicationTest.java
+++ kodilla-advanced-tests/src/test/java/com/kodilla/mockito/homework/WeatherAlertApplicationTest.java
@@ -8,6 +8,29 @@ class WeatherAlertApplicationTest {
     WeatherAlertApp weatherAlertApp = new WeatherAlertApp();
 
     @Test
+    public void shouldAddUserToLocation() {
+        User user1 = Mockito.mock(User.class);
+        Location krakow = Mockito.mock(Location.class);
+        Notification notification = Mockito.mock(Notification.class);
+
+        weatherAlertApp.addSubscriber(user1, krakow);
+        weatherAlertApp.sendNotificationForAllUsers(notification);
+        Mockito.verify(user1, Mockito.times(1)).receiveNotification(notification);
+    }
+
+    @Test
+    public void shouldNotAddUserTwice() {
+        User user1 = Mockito.mock(User.class);
+        Location krakow = Mockito.mock(Location.class);
+        Notification notification = Mockito.mock(Notification.class);
+
+        weatherAlertApp.addSubscriber(user1, krakow);
+        weatherAlertApp.addSubscriber(user1, krakow);
+        weatherAlertApp.sendNotificationForOneLocationUsers(krakow, notification);
+        Mockito.verify(user1, Mockito.times(1)).receiveNotification(notification);
+    }
+
+    @Test
     public void shouldSendNotificationForAllUser() {
         User user1 = Mockito.mock(User.class);
         User user2 = Mockito.mock(User.class);
@@ -22,7 +45,10 @@ class WeatherAlertApplicationTest {
 
         weatherAlertApp.sendNotificationForAllUsers(notification);
         Mockito.verify(user1, Mockito.times(1)).receiveNotification(notification);
+        Mockito.verify(user2, Mockito.times(1)).receiveNotification(notification);
+        Mockito.verify(user3, Mockito.times(1)).receiveNotification(notification);
     }
+
 
     @Test
     public void shouldSendOnlyOneNotificationToMultiTimeSubscriber() {
@@ -58,6 +84,22 @@ class WeatherAlertApplicationTest {
     }
 
     @Test
+    public void shouldNotSendNotificationIfThisLocationDoesNotExistInDatabase() {
+        User user1 = Mockito.mock(User.class);
+        User user2 = Mockito.mock(User.class);
+        Location krakow = Mockito.mock(Location.class);
+        Location rabkazdroj = Mockito.mock(Location.class);
+        Notification notification = Mockito.mock(Notification.class);
+
+        weatherAlertApp.addSubscriber(user1, krakow);
+        weatherAlertApp.addSubscriber(user2, krakow);
+
+        weatherAlertApp.sendNotificationForOneLocationUsers(rabkazdroj, notification);
+        Mockito.verify(user1, Mockito.never()).receiveNotification(notification);
+        Mockito.verify(user2, Mockito.never()).receiveNotification(notification);
+    }
+
+    @Test
     public void shouldRemoveUserFromLocation() {
         User user = Mockito.mock(User.class);
         Location krakow = Mockito.mock(Location.class);
@@ -67,6 +109,20 @@ class WeatherAlertApplicationTest {
         weatherAlertApp.sendNotificationForOneLocationUsers(krakow, notification);
 
         Mockito.verify(user, Mockito.never()).receiveNotification(notification);
+    }
+
+    @Test
+    public void shouldNotRemoveUserIfProvidedLocationDoesNotExistInDatabase() {
+        User user1 = Mockito.mock(User.class);
+        Location krakow = Mockito.mock(Location.class);
+        Location rabkazdroj = Mockito.mock(Location.class);
+        Notification notification = Mockito.mock(Notification.class);
+
+        weatherAlertApp.addSubscriber(user1, krakow);
+
+        weatherAlertApp.removeSubscriberFromLocation(user1, rabkazdroj);
+        weatherAlertApp.sendNotificationForAllUsers(notification);
+        Mockito.verify(user1, Mockito.times(1)).receiveNotification(notification);
     }
 
     @Test
@@ -94,5 +150,18 @@ class WeatherAlertApplicationTest {
         weatherAlertApp.sendNotificationForAllUsers(notification);
 
         Mockito.verify(user, Mockito.never()).receiveNotification(notification);
+    }
+
+    @Test
+    public void shouldNotRemoveLocationFromDatabaseIfLocationDoesNotExistInDatabase() {
+        User user = Mockito.mock(User.class);
+        Location krakow = Mockito.mock(Location.class);
+        Location lodz = Mockito.mock(Location.class);
+        Notification notification = Mockito.mock(Notification.class);
+        weatherAlertApp.addSubscriber(user, krakow);
+        weatherAlertApp.deleteLocation(lodz);
+        weatherAlertApp.sendNotificationForAllUsers(notification);
+
+        Mockito.verify(user, Mockito.times(1)).receiveNotification(notification);
     }
 }
