147291ba5b30d20c7121212bca4bdb41d7215bdb
Karolina Kukula <kukukaro@gmail.com>
2020-11-02 21:49:01 +0100

    Modul 9 Spring  9.4 Car Mocktest

--- kodilla-spring-basic/src/test/java/com/kodilla/spring/basic/spring_configuration/homework/CarConfigurationMockTest.java
+++ kodilla-spring-basic/src/test/java/com/kodilla/spring/basic/spring_configuration/homework/CarConfigurationMockTest.java
@@ -3,34 +3,28 @@ package com.kodilla.spring.basic.spring_configuration.homework;
 import org.junit.Test;
 import org.junit.jupiter.api.Assertions;
 import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.Mockito;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Import;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 public class CarConfigurationMockTest {
 
-    @MockBean
+    @Mock
     SeasonService seasonService;
 
-    @Autowired
+    @InjectMocks
     CarService carService;
 
     @Test
     public void shouldHasLightsTurnedOn() {
         Mockito.when(seasonService.hour()).thenReturn(6);
         Mockito.when(seasonService.season()).thenReturn("Winter");
+
         Car car = carService.createCar();
         boolean isTurnedOn = car.hasHeadLightsTurnedOn();
         Assertions.assertEquals(true, isTurnedOn);
-    }
-
-    @Configuration
-    @Import(CarService.class)
-    static class Config {
-
+        Assertions.assertEquals(SUV.class, car.getClass());
     }
 }
