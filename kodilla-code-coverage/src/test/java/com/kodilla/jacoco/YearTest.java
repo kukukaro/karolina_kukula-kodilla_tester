4b40167687c72180fd8ed361bf08dbd7adcd7dbd
Karolina Kukula <kukukaro@gmail.com>
2020-12-10 17:10:15 +0100

    Modul 15 - 15.2 spotbugs

--- kodilla-code-coverage/src/test/java/com/kodilla/jacoco/YearTest.java
+++ kodilla-code-coverage/src/test/java/com/kodilla/jacoco/YearTest.java
@@ -2,6 +2,7 @@ package com.kodilla.jacoco;
 
 import org.junit.Test;
 
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 public class YearTest {
@@ -17,5 +18,44 @@ public class YearTest {
 
         //then
         assertTrue(isLeapYear);
+    }
+
+    @Test
+    public void shouldBeLeapYearIfDivisibleBy4() {
+
+        //given
+        Year year = new Year(2020);
+
+        //when
+        boolean isLeapYear = year.isLeap();
+
+        //then
+        assertTrue(isLeapYear);
+    }
+
+    @Test
+    public void shouldNotBeLeapYearIfDivisibleBy100() {
+
+        //given
+        Year year = new Year(1900);
+
+        //when
+        boolean isLeapYear = year.isLeap();
+
+        //then
+        assertFalse(isLeapYear);
+    }
+
+    @Test
+    public void shouldNotBeLeapYearIfIsNotDivisibleBy4() {
+
+        //given
+        Year year = new Year(1903);
+
+        //when
+        boolean isLeapYear = year.isLeap();
+
+        //then
+        assertFalse(isLeapYear);
     }
 }
